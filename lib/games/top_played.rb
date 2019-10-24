2f64414da973613f5080a9f56aec86ad49b5261e
Chunky <jasonc@nulogy.com>
2019-10-24 10:13:47 -0400

    Refactor: Cleanup top-played

--- lib/games/top_played.rb
+++ lib/games/top_played.rb
@@ -1,34 +1,19 @@
 class TopPlayed
   def games
-    months_data
-      .product((1..10).to_a)
+    (1..10)
       .lazy
-      .map { |month, page| [month, url_for_month_and_page(month, page)] }
-      .map { |month, url| [month, Utils.read_url(url)] }
-      .map { |month, file| [month, Nokogiri::HTML(file)] }
-      .flat_map { |month, doc| games_for_doc(month, doc) }
+      .map { |page| url_for_month_and_page(page) }
+      .map { |url| Utils.read_url(url) }
+      .map { |file| Nokogiri::HTML(file) }
+      .flat_map { |doc| games_for_doc(doc) }
       .force
-      .each_with_object({}) do |game, memo|
-        memo[game.name] ||= game
-        memo[game.name].players = game.players.merge(memo[game.name].players)
-      end
-      .values
-      .tap { |games| add_player_count(games) }
   end
 
-  def months_data
-    first = Date.parse('2005-01-01')
-    last = last_month
-    (first..last)
-      .select { |d| d.day == 1 }
-      .last(1)
+  def url_for_month_and_page(page)
+    "https://boardgamegeek.com/plays/bygame/start/#{last_month.beginning_of_month}/end/#{last_month.end_of_month}/page/#{page}?sortby=distinctusers"
   end
 
-  def url_for_month_and_page(month, page)
-    "https://boardgamegeek.com/plays/bygame/start/#{month.beginning_of_month}/end/#{month.end_of_month}/page/#{page}?sortby=distinctusers"
-  end
-
-  def games_for_doc(month, doc)
+  def games_for_doc(doc)
     doc.css('.forum_table')[1].css('tr')[1..-1].map do |row|
       link, _, plays = row.css('td')
       anchor = link.css('a')
@@ -39,21 +24,18 @@ class TopPlayed
 
       next if play_count < 1
 
-      game = OpenStruct.new(href: href, name: name, key: key, players: {})
-      game.players[month.to_s] = play_count
-      game
+      OpenStruct.new(
+        href: href,
+        name: name,
+        key: key,
+        player_count: play_count
+      )
     end.compact
   rescue
     []
   end
 
-  def add_player_count(games)
-    games.each do |game|
-      game.player_count = game.players.to_h[last_month.to_s].to_i
-    end
-  end
-
   def last_month
-    @last_month ||= (Date.today - 1.month).beginning_of_month
+    (Date.today - 1.month).beginning_of_month
   end
 end
