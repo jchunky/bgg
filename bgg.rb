b0693561180c2c74171dd75e8de1354029150e8f
Chunky <jasonc@nulogy.com>
2021-05-22 07:39:55 -0400

    Aggressively filter out games

--- bgg.rb
+++ bgg.rb
@@ -17,10 +17,9 @@ class Bgg
     return false if game.rank < 1
     return false if game.play_rank < 1
 
-    return false unless game.was_in_top_100?
+    # return false unless game.was_in_top_100?
+    return false unless game.in_top_100?
     return false if game.year > Bgg::MAX_GAME_YEAR
-
-    # return false unless game.in_top_100?
 
     true
   end
