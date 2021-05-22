57c12aab90502950eef8af972b35edb6e48311b6
Chunky <jasonc@nulogy.com>
2021-05-22 07:45:38 -0400

    --amend

--- bgg.rb
+++ bgg.rb
@@ -19,6 +19,7 @@ class Bgg
 
     # return false unless game.was_in_top_100?
     return false unless game.in_top_100?
+    return false unless game.in_top_100_for_a_year?
     return false if game.year > Bgg::MAX_GAME_YEAR
 
     true
