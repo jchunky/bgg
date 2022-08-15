412e2b0e790f5563c1a8bccdaaea310baac29bc8
Chunky <jasonc@nulogy.com>
2022-08-15 06:15:31 -0400

    Fix calculation of play_rank

--- lib/top_played.rb
+++ lib/top_played.rb
@@ -7,20 +7,20 @@ class TopPlayed < GamepageDownloader
   end
 
   def games_for_doc(doc, page)
-    doc.css(".forum_table")[1].css("tr").map.with_index do |row, i|
-      link = row.css("td")[0]
-      next unless link
+    doc.css(".forum_table")[1].css("tr")
+      .select { |row| row.css("td")[0] }
+      .map.with_index do |row, i|
+        link = row.css("td")[0]
+        anchor = link.css("a")
+        name = Utils.strip_accents(anchor[0].content)
 
-      anchor = link.css("a")
-      name = Utils.strip_accents(anchor[0].content)
-
-      Game.new(
-        href: href = anchor[0]["href"],
-        key: href,
-        name: name,
-        play_rank: ((page - 1) * 100) + i + 1
-      )
-    end
+        Game.new(
+          href: href = anchor[0]["href"],
+          key: href,
+          name: name,
+          play_rank: ((page - 1) * 100) + i + 1
+        )
+      end
   rescue StandardError
     []
   end
--- lib/top_voted.rb
+++ lib/top_voted.rb
@@ -4,21 +4,21 @@ class TopVoted < GamepageDownloader
   end
 
   def games_for_doc(doc, page)
-    doc.css(".collection_table")[0].css("tr").map.with_index do |row, i|
-      rank, _, title, _, rating = row.css("td")
-      next unless rating
+    doc.css(".collection_table")[0].css("tr")
+      .select { |row| row.css("td")[4] }
+      .map.with_index do |row, i|
+        rank, _, title, _, rating = row.css("td")
+        name = Utils.strip_accents(title.css("a")[0].content)
 
-      name = Utils.strip_accents(title.css("a")[0].content)
-
-      Game.new(
-        href: href = title.css("a")[0]["href"],
-        key: href,
-        name: name,
-        rating: rating.content.to_f,
-        year: (title.css("span")[0].content[1..-2].to_i rescue 0),
-        rank: (rank.css("a")[0]["name"].to_i rescue 0),
-        vote_rank: ((page - 1) * 100) + i + 1
-      )
-    end
+        Game.new(
+          href: href = title.css("a")[0]["href"],
+          key: href,
+          name: name,
+          rating: rating.content.to_f,
+          year: (title.css("span")[0].content[1..-2].to_i rescue 0),
+          rank: (rank.css("a")[0]["name"].to_i rescue 0),
+          vote_rank: ((page - 1) * 100) + i + 1
+        )
+      end
   end
 end
