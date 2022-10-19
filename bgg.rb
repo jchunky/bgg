f404228d88971e992f5b09678659bdf95f3f00f3
Chunky <jasonc@nulogy.com>
2022-10-19 05:03:23 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -22,24 +22,13 @@ class Bgg
 
   def display_game?(game)
     return false unless game.year.to_i >= MIN_GAME_YEAR
-
     return false unless game.play_rank > 0
-    # return false unless game.rank > 0
-    # return false unless game.vote_rank > 0
 
     # return game.children_rank >= 1
-    # return game.family_rank >= 1
-    # return game.party_rank >= 1
-    # return game.strategy_rank >= 1
     # return game.thematic_rank >= 1
 
     return false unless game.play_rank.between?(1, 50)
-    # return false unless game.play_rank.between?(1, 200)
-
     return false unless game.rank.between?(1, 200)
-    # return false unless game.rank.between?(1, 500)
-
-    # return false unless game.vote_rank.between?(1, 200)
 
     true
   end
