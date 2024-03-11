31178edadb16239592299404f02f8ff06fbc9b0c
Chunky <jasonc@nulogy.com>
2024-03-11 09:52:33 -0400

    Move replay methods into Concerning block

--- lib/game.rb
+++ lib/game.rb
@@ -35,52 +35,56 @@ class Game < Struct.new(*GameFields::FIELDS.keys, keyword_init: true)
     end
   end
 
-  def replays
-    url = "https://boardgamegeek.com/playstats/thing/#{objectid}/page/#{tenth_percentile_page}"
-    file = Utils.read_file(url, extension: "html")
-    doc = Nokogiri::HTML(file)
-    rows = doc.css(".forum_table td.lf a")
-    if rows.count.zero?
-      0
-    else
-      index = (page_count % 10) / 10.0 * rows.count
-      rows[index].content.to_i
-    end
-  end
-
   private
-
-  def tenth_percentile_page
-    if page_count >= 10 && page_count % 10 == 0
-      (page_count / 10) + 1
-    else
-      (page_count / 10.0).ceil
-    end
-  end
-
-  def page_count
-    url = "https://boardgamegeek.com/playstats/thing/#{objectid}/page/1"
-    file = Utils.read_file(url, extension: "html")
-    doc = Nokogiri::HTML(file)
-    last_page_anchor = doc.css('#maincontent p a[title="last page"]')
-    pagination_anchors = doc.css("#maincontent p a")
-    if last_page_anchor.count >= 1
-      last_page_anchor.first.content.scan(/\d+/).first.to_i
-    elsif pagination_anchors.count >= 2
-      pagination_anchors[-2].content.to_i
-    else
-      1
-    end
-  end
-
-  def objectid
-    href.scan(/\b\d+\b/).first.to_i
-  end
 
   def merge_attr(attr, other)
     value = send(attr)
     other_value = other.send(attr)
 
     value.present? && value != 0 ? value : other_value
+  end
+
+  concerning :Replays do
+    def replays
+      url = "https://boardgamegeek.com/playstats/thing/#{objectid}/page/#{tenth_percentile_page}"
+      file = Utils.read_file(url, extension: "html")
+      doc = Nokogiri::HTML(file)
+      rows = doc.css(".forum_table td.lf a")
+      if rows.count.zero?
+        0
+      else
+        index = (page_count % 10) / 10.0 * rows.count
+        rows[index].content.to_i
+      end
+    end
+
+    private
+
+    def tenth_percentile_page
+      if page_count >= 10 && page_count % 10 == 0
+        (page_count / 10) + 1
+      else
+        (page_count / 10.0).ceil
+      end
+    end
+
+    def page_count
+      url = "https://boardgamegeek.com/playstats/thing/#{objectid}/page/1"
+      file = Utils.read_file(url, extension: "html")
+      doc = Nokogiri::HTML(file)
+      last_page_anchor = doc.css('#maincontent p a[title="last page"]')
+      pagination_anchors = doc.css("#maincontent p a")
+      if last_page_anchor.count >= 1
+        last_page_anchor.first.content.scan(/\d+/).first.to_i
+      elsif pagination_anchors.count >= 2
+        pagination_anchors[-2].content.to_i
+      else
+        1
+      end
+    end
+
+    def objectid
+      href.scan(/\b\d+\b/).first.to_i
+    end
   end
 end
