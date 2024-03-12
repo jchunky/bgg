dc4ece110241ba51003626841e126141e31f9ede
Chunky <jasonc@nulogy.com>
2024-03-12 02:31:14 -0400

    Refactor: inline #fetch_* methods

--- lib/game.rb
+++ lib/game.rb
@@ -59,21 +59,19 @@ class Game
 
   concerning :Replays do
     def replays
-      @replays ||= fetch_replays
+      @replays ||= begin
+        doc = fetch_page_data(tenth_percentile_page)
+        rows = doc.css(".forum_table td.lf a")
+        if rows.count.zero?
+          0
+        else
+          index = (page_count % 10) / 10.0 * rows.count
+          rows[index].content.to_i
+        end
+      end
     end
 
     private
-
-    def fetch_replays
-      doc = fetch_page_data(tenth_percentile_page)
-      rows = doc.css(".forum_table td.lf a")
-      if rows.count.zero?
-        0
-      else
-        index = (page_count % 10) / 10.0 * rows.count
-        rows[index].content.to_i
-      end
-    end
 
     def tenth_percentile_page
       if page_count >= 10 && page_count % 10 == 0
@@ -84,19 +82,17 @@ class Game
     end
 
     def page_count
-      @page_count ||= fetch_page_count
-    end
-
-    def fetch_page_count
-      doc = fetch_page_data(1)
-      last_page_anchor = doc.css('#maincontent p a[title="last page"]')
-      pagination_anchors = doc.css("#maincontent p a")
-      if last_page_anchor.count >= 1
-        last_page_anchor.first.content.scan(/\d+/).first.to_i
-      elsif pagination_anchors.count >= 2
-        pagination_anchors[-2].content.to_i
-      else
-        1
+      @page_count ||= begin
+        doc = fetch_page_data(1)
+        last_page_anchor = doc.css('#maincontent p a[title="last page"]')
+        pagination_anchors = doc.css("#maincontent p a")
+        if last_page_anchor.count >= 1
+          last_page_anchor.first.content.scan(/\d+/).first.to_i
+        elsif pagination_anchors.count >= 2
+          pagination_anchors[-2].content.to_i
+        else
+          1
+        end
       end
     end
 
