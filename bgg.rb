2c10dc6deac86e0cc5ec642ba3df59c60ff3bf8d
Chunky <jasonc@nulogy.com>
2024-05-15 09:47:00 -0400

    Filter out games with same name, but lower rank

--- bgg.rb
+++ bgg.rb
@@ -12,7 +12,7 @@ class Bgg
   MAX_GAME_YEAR = Date.today.year - 5
 
   def display_game?(game)
-    return game.own? && game.rank < 10_000
+    return game.own?
 
     # return false unless game.campaign?
     # return false unless game.solo?
@@ -63,6 +63,11 @@ class Bgg
         hash.merge(by_key(downloader), &method(:merge_hashes))
       end
       .values
+
+    result = result
+      .select { |g| g.rank.positive? }
+      .sort_by(&:rank)
+      .uniq(&:name)
 
     result
       .select { |g| g.play_rank.positive? }
