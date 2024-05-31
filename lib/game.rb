cfe87f3b84c3b29f5eacb94f587dfb258cd31059
Chunky <jasonc@nulogy.com>
2024-05-31 09:47:21 -0400

    Refactor: extract more concerning blocks in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -7,77 +7,18 @@ class Game
     @attributes = Hash.new(0).merge(args)
   end
 
-  def method_missing(method_name, *args)
-    attribute_name = method_name.to_s.chomp("=").to_sym
-    if method_name.to_s.end_with?("=")
-      attributes[attribute_name] = args.first
-    else
-      attributes[attribute_name]
-    end
-  end
-
-  def replays
-    # return 0
-    @replays ||= Downloaders::ReplaysFetcher.new(objectid: key).replays
-  end
-
-  def key
-    @key ||= href.scan(/\b\d+\b/).first.to_i
-  end
-
-  def min_age
-    @min_age ||= (2..18).find { |age| send("age_#{age}?") } || 0
-  end
-
-  def max_playtime
-    @max_playtime ||= (15..360).step(15).find { |time| send("playtime_#{time}?") == true } || 0
-  end
-
-  concerning :Merge do
-    def merge(other)
-      Game.new(attributes.merge(other.attributes)) { |_, a, b| null?(a) ? b : a }
+  concerning :Stats do
+    def replays
+      # return 0
+      @replays ||= Downloaders::ReplaysFetcher.new(objectid: key).replays
     end
 
-    private
-
-    def null?(value)
-      !value || value.zero?
-    end
-  end
-
-  concerning :Categories do
-    included do
-      ::Downloaders::DOWNLOADERS.map(&:prefix).each do |category|
-        define_method("#{category}?") do
-          ranked_in_category?(category)
-        end
-      end
+    def min_age
+      @min_age ||= (2..18).find { |age| send("age_#{age}?") } || 0
     end
 
-    def categories
-      @categories ||= Downloaders::CATEGORIES.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
-    end
-
-    def mechanics
-      @mechanics ||= Downloaders::MECHANICS.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
-    end
-
-    def families
-      @families ||= Downloaders::FAMILIES.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
-    end
-
-    def subdomains
-      @subdomains ||= Downloaders::SUBDOMAINS.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
-    end
-
-    private
-
-    def ranked_in_category?(category)
-      category_rank(category) > 0
-    end
-
-    def category_rank(category)
-      send("#{category}_rank")
+    def max_playtime
+      @max_playtime ||= (15..360).step(15).find { |time| send("playtime_#{time}?") == true } || 0
     end
   end
 
@@ -128,5 +69,66 @@ class Game
     def max_player_count
       @max_player_count ||= (1..12).to_a.reverse.find { |count| send("player_#{count}?") == true } || 0
     end
+  end
+
+  concerning :Categories do
+    included do
+      ::Downloaders::DOWNLOADERS.map(&:prefix).each do |category|
+        define_method("#{category}?") do
+          ranked_in_category?(category)
+        end
+      end
+    end
+
+    def categories
+      @categories ||= Downloaders::CATEGORIES.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
+    end
+
+    def mechanics
+      @mechanics ||= Downloaders::MECHANICS.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
+    end
+
+    def families
+      @families ||= Downloaders::FAMILIES.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
+    end
+
+    def subdomains
+      @subdomains ||= Downloaders::SUBDOMAINS.map(&:prefix).select(&method(:ranked_in_category?)).join(", ")
+    end
+
+    private
+
+    def ranked_in_category?(category)
+      category_rank(category) > 0
+    end
+
+    def category_rank(category)
+      send("#{category}_rank")
+    end
+  end
+
+  concerning :Attributes do
+    def method_missing(method_name, *args)
+      attribute_name = method_name.to_s.chomp("=").to_sym
+      if method_name.to_s.end_with?("=")
+        attributes[attribute_name] = args.first
+      else
+        attributes[attribute_name]
+      end
+    end
+
+    def merge(other)
+      Game.new(attributes.merge(other.attributes)) { |_, a, b| null?(a) ? b : a }
+    end
+
+    private
+
+    def null?(value)
+      !value || value.zero?
+    end
+  end
+
+  def key
+    @key ||= href.scan(/\b\d+\b/).first.to_i
   end
 end
