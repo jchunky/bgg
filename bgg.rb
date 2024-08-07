fbbc364f45d8f3f365f97a21211fbdb74951a875
Jason CheongKeeYou <jasonc@nulogy.com>
2024-08-07 13:04:54 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -30,12 +30,12 @@ class Bgg
     return false if game.war?
     return false if game.wargame?
 
-    # return false unless game.rating.in?(7..)
     # return false unless game.weight.in?(1.5..)
     # return false unless game.year.in?(2010..)
     return false unless game.max_playtime.in?(1..60)
     return false unless game.play_rank.in?(1..)
-    return false unless game.rank.in?(1..5000)
+    return false unless game.rank.in?(1..1000)
+    return false unless game.rating.in?(7..)
     return false unless game.rating_count.in?(1000..)
     return false unless game.unique_users.in?(100..)
     return false unless game.vote_rank.in?(1..5000)
