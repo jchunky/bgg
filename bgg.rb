442d1005bf279e013df578bf33477530731247ed
Jason CheongKeeYou <jasonc@nulogy.com>
2024-09-09 07:55:13 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -14,20 +14,20 @@ class Bgg
     return false unless game.coop? || game.max_player_count == 1
     return false unless game.min_player_count == 1
 
+    # return false if game.legacy?
     # return false if game.narrative_choice?
     # return false if game.storytelling?
     return false if game.action_points?
     return false if game.app?
     return false if game.dexterity?
     return false if game.flicking?
-    return false if game.legacy?
     return false if game.paper_n_pencil?
     return false if game.realtime?
     return false if game.stacking?
     return false if game.traitor?
     return false if game.war?
 
-    # return false unless game.max_playtime.in?(1..60)
+    return false unless game.max_playtime.in?(1..60)
     return false unless game.play_rank.in?(1..)
 
     return false unless game.replays.in?(10..)
