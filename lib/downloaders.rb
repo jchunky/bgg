dc606142b9781ee9b6a029bbc4420fb278b3da68
Jason CheongKeeYou <jasonc@nulogy.com>
2024-09-10 09:22:21 -0400

    Cut player count and play times

--- lib/downloaders.rb
+++ lib/downloaders.rb
@@ -11,10 +11,12 @@ module Downloaders
   MECHANICS = Categories::MECHANICS.map { |prefix, listid, items_per_page| CategoryGames.new(prefix: prefix, listid: listid, items_per_page: items_per_page, object_type: "property") }
   FAMILIES = Categories::FAMILIES.map { |prefix, listid, items_per_page| CategoryGames.new(prefix: prefix, listid: listid, items_per_page: items_per_page, object_type: "family") }
   SUBDOMAINS = Categories::SUBDOMAINS.map { |prefix, listid, items_per_page| CategoryGames.new(prefix: prefix, listid: listid, items_per_page: items_per_page, object_type: "family") }
-  PLAYER_COUNTS = (1..12).map do |count|
+  # PLAYER_COUNTS = (1..12).map do |count|
+  PLAYER_COUNTS = (1..2).map do |count|
     GameSearch.new(prefix: :"player_#{count}", listid: "playerrangetype", search_criteria: "#{SORTBY_RANK}&range[minplayers][max]=#{count}&range[maxplayers][min]=#{count}&playerrangetype=normal")
   end
-  PLAYTIMES = [15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 300, 360].map do |playtime|
+  # PLAYTIMES = [15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 300, 360].map do |playtime|
+  PLAYTIMES = [15, 30, 45, 60, 90].map do |playtime|
     GameSearch.new(prefix: :"playtime_#{playtime}", listid: "playtime", search_criteria: "#{SORTBY_RANK}&range[playtime][max]=#{playtime}")
   end
   AGES = (2..18).map do |age|
