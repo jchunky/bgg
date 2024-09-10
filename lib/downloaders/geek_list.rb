7fc3a670e1cd707d6b104f29d1f1397ca3129bef
Jason CheongKeeYou <jasonc@nulogy.com>
2024-09-10 08:33:31 -0400

    Add back geeklists

---
+++ lib/downloaders/geek_list.rb
@@ -0,0 +1,34 @@
+module Downloaders
+  class GeekList < Struct.new(:listid, :prefix, :reverse_rank, keyword_init: true)
+    def games
+      result = (1..8).flat_map(&method(:games_for_page)).compact.uniq(&:key)
+      result = result.reverse if reverse_rank
+      result.each.with_index do |game, i|
+        game.send("#{prefix}_rank=", i + 1)
+      end
+    end
+
+    private
+
+    def games_for_page(page)
+      url = url_for_page(page)
+      doc = Utils.fetch_json_data(url)
+      games_for_doc(doc, page)
+    end
+
+    def url_for_page(page)
+      "https://api.geekdo.com/api/listitems?listid=#{listid}&page=#{page}"
+    end
+
+    def games_for_doc(doc, page)
+      doc["data"].map do |record|
+        Game.new(
+          href: href = record["item"]["href"],
+          name: record["item"]["name"],
+          rating: record["stats"]["average"].to_f,
+          rank: record["stats"]["rank"].to_i
+        )
+      end
+    end
+  end
+end
