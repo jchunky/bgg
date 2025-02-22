f6307a687a876c20cbb5ae7febd05f832d64d355
Jason CheongKeeYou <jasonc@nulogy.com>
2025-02-22 18:23:00 -0500

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -23,6 +23,7 @@ class Bgg
     # return false unless (1..2.5).cover?(game.weight)
     # return false unless (1..1000).cover?(game.rank)
     # return false unless (1..30).cover?(game.price)
+    return false unless (1..60).cover?(game.playtime)
 
     return false unless (0..5).cover?(game.cost_per_play)
     # return false unless (10..).cover?(game.replays)
