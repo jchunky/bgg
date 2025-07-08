c06a0ed903778d2a55672102317748a080a0cb36
Jason CheongKeeYou <jasonc@nulogy.com>
2025-07-07 23:24:00 -0400

    Update data

--- lib/downloaders/b2go_data.rb
+++ lib/downloaders/b2go_data.rb
@@ -1,6 +1,6 @@
 module Downloaders
-  class BgbData
-    def self.all = @all ||= new
+  class B2goData
+    def self.all =  @all ||= new
 
     def find_data(bgg_game)
       name_resolver.find_bgo_game(bgg_game)
@@ -14,39 +14,36 @@ module Downloaders
 
     def games
       @games ||= File
-        .read("./data/bgb.txt")
+        .read("./data/b2go.txt")
         .split("\n\n")
         .map { |data| build_game(data) }
         .reject { |game| game.name.blank? }
     end
 
     def build_game(data)
-      _, _, name, _, player_count, playtime, rating, weight, preorder, price = data.split("\n")
-
-      preorder = preorder.include?("*PRE-ORDER*")
+      name, p2, _, _, price, player_count, playtime, rating, weight = data.split("\n")
       price = price.delete_prefix("$").to_f
       playtime = playtime.to_i
       rating = rating.to_f
       weight = weight.to_f
+      year, offer_count = p2.split("•").map(&:to_i)
       if player_count.include?("-")
         min_player_count, max_player_count = player_count.split("-").map(&:to_i)
       else
         min_player_count = player_count.to_i
         max_player_count = player_count.to_i
       end
-
       OpenStruct.new(
         rating:,
         weight:,
-        preorder:,
         name:,
+        year:,
+        offer_count:,
         min_player_count:,
         max_player_count:,
         price:,
         playtime:
       )
-    rescue StandardError
-      OpenStruct.new
     end
   end
 end
