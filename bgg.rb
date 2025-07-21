7594e36220575aca6048c21a341233d111c7b21d
Jason CheongKeeYou <jasonc@nulogy.com>
2025-07-21 11:33:05 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -33,11 +33,11 @@ class Bgg
     # return false unless game.bga?
     # return false unless game.bgb?
     # return false unless game.b2go?
-    return false unless game.play_rank?
+    # return false unless game.play_rank?
     # return false unless game.soloable?
     # return false unless game.min_player_count == 1
     # return false unless game.price.to_i >= 1
-    return false unless game.normalized_price.to_f.round < 50
+    # return false unless game.normalized_price.to_f.round < 50
     # return false unless game.playtime < 100
     # return false unless game.weight.round(1) < 3
 
