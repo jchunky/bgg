d5a29f92b5346b2a51e7626b300e9ecf8b30af70
Jason CheongKeeYou <jasonc@nulogy.com>
2025-07-27 02:59:36 -0400

    Refactor: extract method #find_failed_splits

--- lib/downloaders/snakes_data.rb
+++ lib/downloaders/snakes_data.rb
@@ -8,7 +8,7 @@ module Downloaders
         .read("./data/snakes.txt")
         .then { |data| data =  data.gsub(/All Games\nAnnex\nCollege\nTempe\nChicago\nTucson\nCategory\nA\/Z\nSearch\n\n.*\n.*Location/, '') }
         .then { |data| chunk_games(data) }
-        .tap { |data| data.sort_by(&:first).each { |g| p g } }
+        .tap(&method(:find_failed_splits))
         .map { |game_data| build_game(game_data) }
         .reject { |game| game.name.blank? }
     end
@@ -20,21 +20,36 @@ module Downloaders
         .strip
         .split("\n")
         .reject(&:blank?)
-        .slice_after do |line|
-          line = line.downcase
+        .slice_after(&method(:match_location?))
+    end
 
-          line != "blokus 3d" &&
-            line.match?(/\b\d{1,2}[a-f]\b/) ||
-            line.include?("archives") ||
-            line.include?("staff picks") ||
-            line.include?("retired") ||
-            line.include?("new arrivals") ||
-            line == "rpg" ||
-            line == "culled" ||
-            line == "mia" ||
-            line == "post" ||
-            line == "prep"
-        end
+    def find_failed_splits(data)
+      return
+
+      data.each do |g|
+        pairs = g.each_cons(2).select { |a, b| a == b }
+        p g.to_a if pairs.count >= 2
+      end
+    end
+
+    def match_location?(line)
+      line = line.downcase
+
+      return false if line == "blokus 3d"
+
+      line.match?(/\b\d{1,2}[a-f]\b/) ||
+        line.include?("archives") ||
+        line.include?("new arrivals") ||
+        line.include?("retired") ||
+        line.include?("staff picks") ||
+        line == "1Ω" ||
+        line == "culled" ||
+        line == "mia" ||
+        line == "post" ||
+        line == "prep" ||
+        line == "rpg" ||
+        line == "sickbay" ||
+        false
     end
 
     def build_game(game_data)
