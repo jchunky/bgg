cf4e08b4a783791ae0fd0cf96a5ee9719c532d21
Jason CheongKeeYou <jasonc@nulogy.com>
2025-09-22 23:25:30 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -12,7 +12,7 @@ Dir["lib/**/*.rb"].each { |f| require_relative f }
 class Bgg
   def display_game?(game)
     return false unless !game.banned?
-    # return false unless game.!played?
+    # return false unless !game.played?
     return false unless !game.played? || game.learned?
 
     # return false unless game.one_player?
@@ -24,8 +24,9 @@ class Bgg
     # S&L with Natasha and boys
     return false unless game.snakes?
     return false unless game.competitive?
-    return false unless game.weight.round(1) <= 2.2
-    return false unless game.playtime < 45
+    # return false unless game.weight.round(1) <= 2.2
+    return false unless game.playtime.between?(1, 44)
+    return false unless !game.snakes_category.between?(1, 19)
 
     # BGA (team time)
     # return false unless game.bga?
