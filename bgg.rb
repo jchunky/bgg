18ad6b2ace5bb01f1202aae51f261b6d4f2409b6
Jason CheongKeeYou <jasonc@nulogy.com>
2025-10-13 10:46:21 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -12,7 +12,7 @@ Dir["lib/**/*.rb"].each { |f| require_relative f }
 class Bgg
   def display_game?(game)
     # return false unless !game.banned?
-    # return false unless !game.played?
+    return false unless !game.played?
     # return false unless !game.played? || game.learned?
 
     # S&L with Natasha and boys
@@ -35,13 +35,13 @@ class Bgg
     # return false unless !game.party?
     # return false unless game.family?
 
-    # return false unless game.bga?
+    return false unless !game.bga?
     # return false unless game.bgb?
     # return false unless game.b2go?
     # return false unless game.snakes?
 
     # return false unless game.couples?
-    return false unless game.couples_rank.between?(1, 100)
+    # return false unless game.couples_rank.between?(1, 100)
     # return false unless game.solo?
     # return false unless game.solo_rank.between?(1, 100)
     # return false unless (
@@ -56,12 +56,12 @@ class Bgg
 
     # return false unless game.max_player_count >= 4
     # return false unless game.min_player_count == 1
-    return false unless game.normalized_price >= 1
+    # return false unless game.normalized_price >= 1
     # return false unless game.normalized_price.between?(1, 49)
-    # return false unless game.play_rank.between?(1, 200)
+    return false unless game.play_rank.between?(1, 50)
     # return false unless game.play_rank?
     # return false unless game.playtime.between?(1, 44)
-    return false unless game.rank.between?(1, 500)
+    # return false unless game.rank.between?(1, 500)
     # return false unless game.rank.to_i > 0
     # return false unless game.soloable?
     # return false unless game.thematic?
