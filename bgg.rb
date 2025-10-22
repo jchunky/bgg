4e2fa2ce80ddd852b4ce79b81b97253c8cd3b2fc
Jason CheongKeeYou <jasonc@nulogy.com>
2025-10-22 08:05:50 -0400

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -12,7 +12,7 @@ Dir["lib/**/*.rb"].each { |f| require_relative f }
 class Bgg
   def display_game?(game)
     # return false unless !game.banned?
-    return false unless !game.played?
+    return false unless game.played?
     # return false unless !game.played? || game.learned?
 
     # S&L with Natasha and boys
@@ -44,23 +44,23 @@ class Bgg
     # return false unless game.couples_rank.between?(1, 100)
     # return false unless game.solo?
     # return false unless game.solo_rank.between?(1, 100)
-    return false unless game.couples_rank.between?(1, 100) || game.solo_rank.between?(1, 100)
+    # return false unless game.couples_rank.between?(1, 100) || game.solo_rank.between?(1, 100)
 
     # return false unless game.max_player_count >= 4
     # return false unless game.min_player_count == 1
-    return false unless game.normalized_price >= 1
+    # return false unless game.normalized_price >= 1
     # return false unless game.normalized_price.between?(1, 49)
     # return false unless game.play_rank.between?(1, 50)
     return false unless game.play_rank?
     # return false unless game.player_count_range.cover?(2)
     # return false unless game.playtime.between?(1, 44)
-    return false unless game.rank.between?(1, 500)
+    # return false unless game.rank.between?(1, 500)
     # return false unless game.rank.to_i > 0
     # return false unless !game.soloable?
     # return false unless game.thematic?
     # return false unless game.vote_rank.between?(1, 1000)
     # return false unless game.votes_per_year >= 3000
-    return false unless game.votes_per_year_rank.between?(1, 1000)
+    # return false unless game.votes_per_year_rank.between?(1, 1000)
     # return false unless game.weight.round(1) <= 2.3
     # return false unless game.year >= Time.now.year - 5
 
