51f7bfae690d797062b48185240cf8b0e990e268
Jason CheongKeeYou <jasonc@nulogy.com>
2025-11-26 01:59:55 -0500

    Update b2go data

--- bgg.rb
+++ bgg.rb
@@ -12,13 +12,13 @@ Dir["lib/**/*.rb"].each { |f| require_relative f }
 class Bgg
   def display_game?(game)
     # return false unless !game.banned?
-    # return false unless !game.played?
+    return false unless !game.played?
     # return false unless !game.played? || game.learned?
 
     # Popular 1-player
-    return false unless game.solo_rank.between?(1, 100)
-    return false unless game.rank.between?(1, 500)
-    return false unless game.soloable?
+    # return false unless game.solo_rank.between?(1, 100)
+    # return false unless game.rank.between?(1, 500)
+    # return false unless game.soloable?
 
     # Popular 2-player
     # return false unless game.couples_rank.between?(1, 100)
@@ -57,7 +57,7 @@ class Bgg
 
     # return false unless game.bga?
     # return false unless game.bgb?
-    # return false unless game.b2go?
+    return false unless game.b2go?
     # return false unless game.snakes?
 
     # return false unless game.couples?
@@ -65,7 +65,7 @@ class Bgg
     # return false unless game.solo?
     # return false unless game.solo_rank.between?(1, 100)
 
-    # return false unless game.max_player_count >= 4
+    # return false unless game.max_player_count >= 5
     # return false unless game.min_player_count == 1
     # return false unless game.normalized_price >= 1
     # return false unless game.normalized_price.between?(1, 49)
@@ -75,7 +75,7 @@ class Bgg
     # return false unless game.playtime.between?(1, 44)
     # return false unless game.rank.between?(1, 500)
     # return false unless game.rank.to_i > 0
-    # return false unless game.soloable?
+    return false unless game.soloable?
     # return false unless game.thematic?
     # return false unless game.vote_rank.between?(1, 1000)
     # return false unless game.votes_per_year >= 3000
