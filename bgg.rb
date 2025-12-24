85d9298993c92e4f4c43fd40e7eeee70dfbaff6e
Jason CheongKeeYou <jasonc@nulogy.com>
2025-12-24 01:36:23 -0500

    Update filter criteria

--- bgg.rb
+++ bgg.rb
@@ -12,26 +12,39 @@ Dir["lib/**/*.rb"].each { |f| require_relative f }
 class Bgg
   def display_game?(game)
     # return false unless !game.banned?
-    return false unless game.played?
+    # return false unless !game.played?
     # return false unless !game.played? || game.learned?
 
+    # Solo games to play
+    # return false unless game.played?
+    # return false unless game.normalized_price >= 1
+    # return false unless game.play_rank?
+    # return false unless game.soloable?
+
+    # Solo games to acquire
+    return false unless game.max_player_count.between?(1, 2)
+    return false unless game.normalized_price >= 1
+    return false unless game.play_rank?
+    return false unless (game.min_player_count == 1 || game.coop?)
+
+    # return false unless game.b2go?
     # return false unless game.bga?
     # return false unless game.bgb?
-    # return false unless game.b2go?
     # return false unless game.snakes?
 
+    # return false unless game.min_player_count == 1
     # return false unless game.max_player_count.between?(1, 2)
     # return false unless game.max_player_count >= 5
-    # return false unless game.min_player_count == 1
-    return false unless game.normalized_price >= 1
+    # return false unless game.normalized_price >= 1
     # return false unless game.normalized_price < 30
-    return false unless game.play_rank?
+    # return false unless game.offer_count.to_i >= 10
+    # return false unless game.one_player? || game.coop?
+    # return false unless game.play_rank?
     # return false unless game.player_count_range.cover?(2)
     # return false unless game.playtime.between?(1, 44)
     # return false unless game.rank.between?(1, 500)
-    # return false unless game.offer_count.to_i >= 10
     # return false unless game.rank.to_i > 0
-    return false unless game.soloable?
+    # return false unless game.soloable?
     # return false unless game.thematic?
     # return false unless game.vote_rank.between?(1, 1000)
     # return false unless game.votes_per_year_rank.between?(1, 1000)
