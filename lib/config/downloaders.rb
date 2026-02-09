5466f64a2cfb1b4fc16780561f9395c40dcbdffa
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-09 02:09:21 -0500

    Update list of one player games

--- lib/config/downloaders.rb
+++ lib/config/downloaders.rb
@@ -11,6 +11,7 @@ module Downloaders
   NO_EXPANSIONS = "nosubtypes[]=boardgameexpansion"
   SORTBY_RANK = "#{NO_EXPANSIONS}&sort=rank"
   SORTBY_VOTES = "#{NO_EXPANSIONS}&sort=numvoters&sortdir=desc"
+  ONE_PLAYER_GAMES = "#{SORTBY_RANK}&range[minplayers][max]=1"
   CATEGORIES = Categories::CATEGORIES.map do |prefix, listid, items_per_page, object_type|
     CategoryGames.new(prefix:, listid:, items_per_page:, object_type:)
   end
@@ -19,6 +20,7 @@ module Downloaders
   end
   DOWNLOADERS = [
     GameSearch.new(prefix: :bgg, listid: "rank", search_criteria: SORTBY_RANK),
+    GameSearch.new(prefix: :one_player_games, listid: "minplayers", search_criteria: ONE_PLAYER_GAMES),
     # GameSearch.new(prefix: :vote, listid: "numvoters", search_criteria: SORTBY_VOTES),
     GeekList.new(prefix: :ccc, listid: 370740, reverse_rank: false), # December 2025
     GeekList.new(prefix: :couples, listid: 353032, reverse_rank: false), # 2024
