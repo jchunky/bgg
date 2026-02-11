6daf75edcccfb8494aabe1830f445f0d3af63713
Jason CheongKeeYou <jasonc@nulogy.com>
2026-02-11 01:40:33 -0500

    Refactor

--- lib/config/game_lists.rb
+++ lib/config/game_lists.rb
@@ -1,43 +1,19 @@
 module Config
   module GameLists
-    def self.data
-      @data ||= YAML.load_file("config/game_lists.yml")
-    end
+    class << self
+      def b2go_overrides = data["b2go_overrides"]
+      def banned_categories = data["banned_categories"].map(&:to_sym)
+      def banned_games = data["banned_games"]
+      def banned_series = data["banned_series"]
+      def ccc_overrides = data["ccc_overrides"]
+      def child_overrides = data["child_overrides"]
+      def keep = data["keep"]
+      def skirmish_games = data["skirmish_games"]
+      def weight_overrides = data["weight_overrides"]
 
-    def self.keep
-      data["keep"]
-    end
+      private
 
-    def self.banned_games
-      data["banned_games"]
-    end
-
-    def self.banned_series
-      data["banned_series"]
-    end
-
-    def self.banned_categories
-      data["banned_categories"].map(&:to_sym)
-    end
-
-    def self.b2go_overrides
-      data["b2go_overrides"]
-    end
-
-    def self.ccc_overrides
-      data["ccc_overrides"]
-    end
-
-    def self.child_overrides
-      data["child_overrides"]
-    end
-
-    def self.skirmish_games
-      data["skirmish_games"]
-    end
-
-    def self.weight_overrides
-      data["weight_overrides"]
+      def data = @data ||= YAML.load_file("config/game_lists.yml")
     end
   end
 end
--- lib/models/game.rb
+++ lib/models/game.rb
@@ -55,6 +55,10 @@ module Models
         Models::Game.new(attributes.merge(other.attributes)) { |_, a, b| null?(a) ? b : a }
       end
 
+      def key
+        @key ||= name.to_s.downcase.gsub(/[^\w\s]/, "").squish
+      end
+
       private
 
       def null?(value)
@@ -64,23 +68,22 @@ module Models
 
     concerning :GameData do
       def bgb? = (bgb == true && !preorder?)
+      def competitive? = group == "competitive"
       def crowdfunded? = kickstarter? || gamefound? || backerkit?
-      def play_rank? = (play_rank > 0)
-      def played? = (played == true)
-      def preorder? = (preorder == true)
-      def snakes? = snakes == true
       def learned? = LEARNED.include?(name)
-      def replayed? = REPLAYED.include?(name)
-      def played? = PLAYED.include?(name)
-      def soloable? = max_player_count == 1 || (coop? && min_player_count == 1)
       def normalized_price = (bgb_price > 0 ? bgb_price : price).to_f.round
+      def one_player? = max_player_count == 1
+      def play_rank? = (play_rank > 0)
+      def played? = PLAYED.include?(name)
       def player_count = ([min_player_count, max_player_count].compact.uniq.join("-"))
       def player_count_range = (min_player_count..max_player_count)
+      def preorder? = (preorder == true)
+      def replayed? = REPLAYED.include?(name)
+      def snakes? = snakes == true
       def snakes_category = snakes_location.to_i
       def snakes_location_label = null?(snakes_location) ? nil : snakes_location
-      def one_player? = max_player_count == 1
+      def soloable? = max_player_count == 1 || (coop? && min_player_count == 1)
       def two_player? = max_player_count == 2
-      def competitive? = group == "competitive"
 
       def votes_per_year
         days_published = ((Time.now.year - year.to_i) * 365) + Time.now.yday
@@ -99,12 +102,6 @@ module Models
         end
       end
 
-      def banned?
-        return true if banned_game?
-        return true if banned_series?
-
-        Config::GameLists.banned_categories.any? { send("#{_1}?") }
-      end
 
       def min_player_count
         return 1 if one_player_game_1?
@@ -115,69 +112,29 @@ module Models
     end
 
     concerning :Customize do
-      def keep?
-        Config::GameLists.keep.include?(name)
-      end
-
-      def banned_game?
-        Config::GameLists.banned_games.include?(name)
-      end
-
-      def banned_series?
-        Config::GameLists.banned_series.any? { name.start_with?(_1) }
-      end
-
-      def b2go?
-        b2go == true || Config::GameLists.b2go_overrides.include?(name)
-      end
-
-      def ccc?
-        super || Config::GameLists.ccc_overrides.include?(name)
-      end
-
-      def weight
-        Config::GameLists.weight_overrides.fetch(name, super)
-      end
-
-      def child?
-        super || Config::GameLists.child_overrides.include?(name)
-      end
-
-      def skirmish?
-        Config::GameLists.skirmish_games.include?(name)
-      end
-    end
-
-    def key
-      @key ||= name.to_s.downcase.gsub(/[^\w\s]/, "").squish
+      def b2go? = b2go == true || Config::GameLists.b2go_overrides.include?(name)
+      def banned? = banned_game? || banned_series? || Config::GameLists.banned_categories.any? { send("#{_1}?") }
+      def banned_game? = Config::GameLists.banned_games.include?(name)
+      def banned_series? = Config::GameLists.banned_series.any? { name.start_with?(_1) }
+      def ccc? = super || Config::GameLists.ccc_overrides.include?(name)
+      def child? = super || Config::GameLists.child_overrides.include?(name)
+      def keep? = Config::GameLists.keep.include?(name)
+      def skirmish? = Config::GameLists.skirmish_games.include?(name)
+      def weight = Config::GameLists.weight_overrides.fetch(name, super)
     end
 
     concerning :Display do
       def displayable?
-        return false if too_heavy?
+        return false if weight.round(1) > 2.2
         return false if played?
-        return false unless available_somewhere?
+        return false unless b2go? || snakes? || learned?
         return true if learned?
         return true if keep?
         return false if campaign?
         return false if banned?
-        return false unless solo_playable?
+        return false unless min_player_count == 1
 
         true
-      end
-
-      private
-
-      def too_heavy?(max_weight: 2.2)
-        weight.round(1) > max_weight
-      end
-
-      def available_somewhere?
-        b2go? || snakes? || learned?
-      end
-
-      def solo_playable?
-        min_player_count == 1
       end
     end
   end
