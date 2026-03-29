95b1cf9e3aa9b58a1f8e394393c55d9756e2033f
Jason CheongKeeYou <jasonc@nulogy.com>
2026-03-29 09:50:05 -0400

    Update filter criteria

--- lib/models/game.rb
+++ lib/models/game.rb
@@ -118,15 +118,15 @@ module Models
 
     concerning :Display do
       def displayable?
-        # return false if weight.round(1) > 2.2
+        return false if weight.round(1) > 2.2
         return false if played?
         return false unless b2go?
-        # return true if learned?
-        # return true if keep?
+        return true if learned?
+        return true if keep?
         # return false if campaign?
-        # return false if banned?
+        return false if banned?
         return false if player_count.min != 1
-        return false unless soloable?
+        # return false unless soloable?
         # return false if price == 0
 
         true
